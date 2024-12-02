98c6b79990b2ad706578c5cc6427cbd661c1951d
Mazen <Mazenessam2011@outlook.com>
2024-12-02 13:21:26 +0200

    make the game interactive with mouse and change the piece position

--- game.py
+++ game.py
@@ -1,36 +1,50 @@
 import pygame
 import sys
-from Model import QueenBee, Beetle, Grasshopper, Spider, SoldierAnt
-from constant import WIDTH, HEIGHT, WHITE, RADIUS
-from hex import draw_grid
+from Model import QueenBee
+from constant import WIDTH, HEIGHT, WHITE
+from hex import draw_grid, get_clicked_hex
 # init pygame
 pygame.init()
 
 # set up the screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hello World")
-# fill screen with white
-screen.fill(WHITE)
 
 # draw grid
 tiles = draw_grid(screen, rows=16, cols=19)
 QueenBee1 = QueenBee("White")
-QueenBee1.draw(screen, tiles[141].center)
-# for tile in tiles:
-#     print(tile.position)
-# main loop
+tiles[141].add_piece(QueenBee1)
+
+selected_tile = None
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            clicked_tile = get_clicked_hex(screen, tiles, mouse_pos)
+
+            if clicked_tile:
+                if selected_tile is None:
+                    if clicked_tile.has_pieces():
+                        selected_tile = clicked_tile
+                else:
+                    selected_tile.move_piece(clicked_tile)
+                    selected_tile = None
+
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
 
+    screen.fill(WHITE)
+    for tile in tiles:
+        tile.draw(screen)
     # update the display
     pygame.display.flip()
 # quit pygame
 
 pygame.quit()
 sys.exit()
+
