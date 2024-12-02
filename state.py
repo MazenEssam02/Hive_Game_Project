455b078fb569fc2cd14ed8d66ef0e69af8c18c0e
georg <george25gehamnew@gmail.com>
2024-12-03 00:43:37 +0200

    Add turntimer class to start integrate timer for each turn

--- state.py
+++ state.py
@@ -1,20 +1,16 @@
 import pygame
-from constant import TIMER_EVENT , GAME_STATES
+from constant import TIMER_EVENT
 
 class state:
-    def __init__(self , current_state):
-        if current_state in GAME_STATES:
-            self.current_state = current_state
-        else:
-            self.current_state = ''
+    def __init__(self ):
+        self.start_game()
     def change_turn(self):
-        pygame.time.set_timer(TIMER_EVENT , 1000)
+        # pygame.time.set_timer(TIMER_EVENT , 1000)
         if self.current_state == 'WHITE TURN' :
             self.current_state = 'BLACK TURN'
         elif self.current_state == 'BLACK TURN':
-            self.current_state == 'WHITE TURN'
+            self.current_state = 'WHITE TURN'
     def start_game(self):
-        self.turn = 1
         self.current_state = 'WHITE TURN'
         #drigger event in Timer_event every 1sec
         pygame.time.set_timer(TIMER_EVENT , 1000)
@@ -24,3 +20,15 @@ class state:
         elif self.current_state == 'BLACK TURN':
             return 'BLACK TURN'
     
+class TurnTimer():
+    def __init__(self):
+        self.turn_time = 180
+        self.timer_event = TIMER_EVENT
+        pygame.time.set_timer(self.timer_event , 1000)
+    def update_timer(self):
+        if self.turn_time > 0:
+            self.turn_time -= 1
+    def reset_timer(self):
+        self.turn_time = 180
+    def get_time(self):
+        return self.turn_time
